fd33bb498c23b3f42a9ebbf3adb91cd64862bfd8
Shark <Shark@192.168.1.103>
2019-01-19 20:56:21 +0800

    Cp9 一半休息

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -5,4 +5,6 @@ from article import views
 app_name = 'article'
 urlpatterns = [
     path('', views.article, name='article'),
+    path('articleCreate/', views.articleCreate, name='articleCreate'),
+    path('articleRead/<int:articleId>/', views.articleRead, name='articleRead'),
 ]
--- blog/article/views.py
+++ blog/article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from article.models import Article,Comment
-
+from article.forms import ArticleForm
 
 def article(request):
     '''
@@ -12,3 +12,37 @@ def article(request):
         articles.update({article:Comment.objects.filter(article=article)})
     context = {'articles':articles}
     return render(request, 'article/article.html', context)
+
+def articleCreate(request):
+    '''
+    Create a new article instance
+        1. If method is GET, render an empty form
+        2. If method is POST, perform form validation and display error messages if the form is invalid
+        3. Save the form to the model and redirect the user to the article page
+    '''
+    template = 'article/articleCreate.html'
+    if request.method == 'GET':
+        return render(request, template, {'articleForm':ArticleForm()})
+    
+    #post
+    articleForm = ArticleForm(request.POST)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm':articleForm})
+    
+    articleForm.save()
+    messages.success(request, '文章已新增')
+    return redirect('article:article')
+
+def articleRead(request, articleId):
+    '''
+    Read an article
+        1. Get the "article" instance using "articleId"; redirect to the 404 page if not found
+        2. Render the articleRead template with the article instance and its
+           associated comments
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    context = {
+        'article':article,
+        'comments': Comment.objects.filter(article=article)
+    }
+    return render(request, 'article/articleRead.html', context)
